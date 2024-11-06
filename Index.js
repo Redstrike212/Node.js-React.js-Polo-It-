2aea1a50e998d93ed30494aedec077e8af4b2bcb
Magno <islermagno@ca.frre.utn.edu.ar>
2024-11-06 14:36:43 -0300

    modified:   Index.js 	modified:   package.json 	modified:   src/db/database.js 	new file:   src/models/User.js

--- Index.js
+++ Index.js
@@ -1,6 +1,17 @@
 const server = require('./src/server');
+const mongoose = require('./src/db/database')
 require('dotenv').config()
 
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, console.log(`Server listened in port ${PORT}`));
+async function main() {
+    try {
+        await mongoose.connection
+        console.log('Conexion con la Base de Datos')
+        server.listen(PORT, console.log(`Server listened in port ${PORT}`))
+    } catch (error) {
+        console.error('Error al conectarse a la base de datos ', error)
+    }
+}
+
+main()
--- src/db/database.js
+++ src/db/database.js
@@ -1,7 +1,15 @@
-const users = [
-    // { id: 1, name: "magno", username: "magno-DevJunior", email: "islermagno@gmail.com" },
-    // { id: 2, name: "maria", username: "maria-DevJunior", email: "islermagno@gmail.com" },
-    // { id: 3, name: "thiago", username: "thiago-DevJunior", email: "islermagno@gmail.com" }
-]
+const mongoose = require('mongoose')
 
-module.exports = users
+const mongoUrl = 'mongodb+srv://islermagno:Magn01sler@clustertest.0nyc0.mongodb.net/'
+
+mongoose.connect(mongoUrl)
+
+const db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'Connection error: '))
+
+db.once('open', () => {
+    console.log("Connectes to MongoDb")
+});
+
+module.exports = mongoose
